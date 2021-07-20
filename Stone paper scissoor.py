7fe19c44ea09f9a744e52d1010d45f529de6ba4a
Danish <64760966+Danishmahajan@users.noreply.github.com>
2021-07-20 17:46:33 +0530

    Update and rename Stone paper sciccor.py to Stone paper scissoor.py

--- Stone paper scissoor.py
+++ Stone paper scissoor.py
@@ -3,28 +3,33 @@ player1count = 0
 player2count = 0
 for i in range(1 ,Turns+1):
     print(" ===============   Round" ,i,  "Starts    ===============")
-    firstTurn = input("Its Player 1 turn :")
+    p1 = input("Its Player 1 turn :")
     print ("\033[A                                          ")
     print("Player 1 Input is locked up ...............")
-    secondTurn = input("Its Player 2 turn :")
+    p2 = input("Its Player 2 turn :")
 
-    if(firstTurn == "stone" and secondTurn == "scissor" or firstTurn == "scissor" and secondTurn == "paper" or firstTurn == "paper" and secondTurn == "stone"):
+    if(p1 == "stone" and p2 == "scissor" or p1 == "scissor" and p2 == "paper" or p1 == "paper" and p2 == "stone"):
         print("(:-) ===============    Player 1 wins the  battle    =============== :-)")
 
         player1count += 1
 
-    elif(firstTurn == "scissor" and secondTurn == "stone" or firstTurn == "paper" and secondTurn == "scissor" or firstTurn == "stone" and secondTurn == "paper"):
+    elif(p1 == "scissor" and p2 == "stone" or p1 == "paper" and p2 == "scissor" or p1 == "stone" and p2 == "paper"):
         print(":-) ===============   Player 2 wins the battle   =============== :-)")
         player2count += 1
 
-    elif(firstTurn == secondTurn):
+    elif(p1 == p2):
         print(" ===============   Its DRAW ,","NO points in this Round",i,"   ===============")
 
     else:
         print(" ===============   Invalid INPUT   ===============")
 
-    print("---------------    RECORD TILL NOW =>    ---------------")
-    print("Player 1 wins",player1count,"Rounds   and  Player 2 wins" ,player2count,"Rounds")
+    if(player1count==(player2count+2)):
+        print("Player 1 wins the tournament with max. wins.................................")
+        break
+    elif(player2count==(player1count+2)):
+        print("player 2 wins the tournament with max. wins.................................")
+        break
+
 print("==================    SCOREBOARD     ==================")
 if(player1count > player2count):
     print("===============    CONGRATULATIONS,Player 1 won SERIES with " ,player1count,":",player2count,"    ===============")
@@ -34,4 +39,3 @@ elif(player2count > player1count):
 
 elif(player1count == player2count):
     print("===============    ITS TIE    ===============")
-
